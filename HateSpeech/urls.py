4782fa070b774124e8cd9a3afa584dda45298206
Bruno Teixeira <bruno.teixeira@in8.com.br>
2022-05-20 15:11:32 -0300

    feat: ajustado rota principal

--- HateSpeech/urls.py
+++ HateSpeech/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import HateSpeech.core.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    # padrão
+    path('', HateSpeech.core.views.home, name='home'), path('', include('django.contrib.auth.urls')),
 ]
